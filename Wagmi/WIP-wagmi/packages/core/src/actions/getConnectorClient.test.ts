122176965020c852b86810b316fea5d03a2a6673
David J. Desjardins <monsterunits@gmail.com>
2024-04-19 15:39:15 -0400

    add other example apps

---
+++ Wagmi/WIP-wagmi/packages/core/src/actions/getConnectorClient.test.ts
@@ -0,0 +1,58 @@
+import { address, config } from '@wagmi/test'
+import { expect, test } from 'vitest'
+
+import { connect } from './connect.js'
+import { disconnect } from './disconnect.js'
+import { getConnectorClient } from './getConnectorClient.js'
+
+const connector = config.connectors[0]!
+
+test('default', async () => {
+  await connect(config, { connector })
+  await expect(getConnectorClient(config)).resolves.toBeDefined()
+  await disconnect(config, { connector })
+})
+
+test('parameters: connector', async () => {
+  const connector2 = config.connectors[1]!
+  await connect(config, { connector })
+  await connect(config, { connector: connector2 })
+  await expect(getConnectorClient(config, { connector })).resolves.toBeDefined()
+  await disconnect(config, { connector })
+  await disconnect(config, { connector: connector2 })
+})
+
+test.todo('custom connector client')
+
+test('behavior: account address is checksummed', async () => {
+  await connect(config, { connector })
+  const account = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+  const client = await getConnectorClient(config, { account })
+  expect(client.account.address).toMatchInlineSnapshot(
+    '"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"',
+  )
+  expect(client.account.address).not.toBe(account)
+  await disconnect(config, { connector })
+})
+
+test('behavior: not connected', async () => {
+  await expect(
+    getConnectorClient(config),
+  ).rejects.toThrowErrorMatchingInlineSnapshot(`
+    "Connector not connected.
+
+    Version: @wagmi/core@x.y.z"
+  `)
+})
+
+test('behavior: account does not exist on connector', async () => {
+  await connect(config, { connector })
+  await expect(
+    getConnectorClient(config, { account: address.usdcHolder }),
+  ).rejects.toThrowErrorMatchingInlineSnapshot(`
+    "Account \\"0x5414d89a8bF7E99d732BC52f3e6A3Ef461c0C078\\" not found for connector \\"Mock Connector\\".
+
+    Version: @wagmi/core@x.y.z"
+  `)
+  await disconnect(config, { connector })
+})
---
+++ Wagmi/WIP-wagmi/packages/register-tests/react/src/useSwitchChain.test-d.ts
@@ -0,0 +1,25 @@
+import { expectTypeOf, test } from 'vitest'
+import { useSwitchChain } from 'wagmi'
+import { type Chain, celo, mainnet, optimism } from 'wagmi/chains'
+
+import { type ChainId } from './config.js'
+
+test('default', () => {
+  const { chains, switchChain } = useSwitchChain()
+
+  expectTypeOf(chains[0]).toEqualTypeOf<Chain | typeof celo>()
+  expectTypeOf(chains[2]).toEqualTypeOf<Chain | typeof optimism>()
+  expectTypeOf(chains[5]).toEqualTypeOf<Chain | undefined>()
+
+  switchChain(
+    { chainId: 1 },
+    {
+      onSuccess(data) {
+        expectTypeOf(data).toEqualTypeOf(mainnet)
+      },
+    },
+  )
+
+  type Result = Parameters<typeof switchChain>[0]
+  expectTypeOf<Result['chainId']>().toEqualTypeOf<ChainId>()
+})
