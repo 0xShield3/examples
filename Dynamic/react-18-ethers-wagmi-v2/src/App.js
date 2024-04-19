513c151675828783df38aad7f7957f73965a9681
David J. Desjardins <monsterunits@gmail.com>
2024-04-19 15:31:00 -0400

    add dynamic

---
+++ Dynamic/react-18-ethers-wagmi-v2/src/App.js
@@ -0,0 +1,44 @@
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { EthersExtension } from "@dynamic-labs/ethers-v5";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { createConfig, WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { http } from "viem";
+import { mainnet } from "viem/chains";
+
+const config = createConfig({
+  chains: [mainnet],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+        walletConnectorExtensions: [EthersExtension],
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>
+            <DynamicWidget />
+          </DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </DynamicContextProvider>
+  );
+};
+
+export default App;
