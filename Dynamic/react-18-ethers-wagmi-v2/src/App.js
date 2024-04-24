5330d81b390f6ea707b59e37061ad1ef846a74af
David J. Desjardins <monsterunits@gmail.com>
2024-04-24 17:43:36 -0400

    Add example app

--- Dynamic/react-18-ethers-wagmi-v2/src/App.js
+++ Dynamic/react-18-ethers-wagmi-v2/src/App.js
@@ -7,9 +7,11 @@ import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { createConfig, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { Shield3Provider } from '@shield3/react-sdk'
+import Signer from './Signer'
 import { http } from "viem";
 import { mainnet } from "viem/chains";
+
 
 const config = createConfig({
   chains: [mainnet],
@@ -23,9 +25,10 @@ const queryClient = new QueryClient();
 
 const App = () => {
   return (
+    <Shield3Provider apiKey={process.env.REACT_APP_SHIELD3_API_KEY || 'undefined-shield3-api-key'} chainId={mainnet.id}>
     <DynamicContextProvider
       settings={{
-        environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+        environmentId: process.env.REACT_APP_DYNAMIC_PROJECT_ID,
         walletConnectorExtensions: [EthersExtension],
         walletConnectors: [EthereumWalletConnectors],
       }}
@@ -34,10 +37,12 @@ const App = () => {
         <QueryClientProvider client={queryClient}>
           <DynamicWagmiConnector>
             <DynamicWidget />
+            <Signer/>
           </DynamicWagmiConnector>
         </QueryClientProvider>
       </WagmiProvider>
     </DynamicContextProvider>
+    </Shield3Provider>
   );
 };
 
