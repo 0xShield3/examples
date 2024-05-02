6e20c6af0de9c908aa689e436a31751bc7f8190e
David J. Desjardins <monsterunits@gmail.com>
2024-05-02 10:08:20 -0400

    fix style

--- Dynamic/nextjs-viem-wagmi-v2/app/page.tsx
+++ Dynamic/nextjs-viem-wagmi-v2/app/page.tsx
@@ -7,10 +7,12 @@ import Signer from "./Signer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
       <Shield3Provider apiKey={process.env.NEXT_PUBLIC_SHIELD3_API_KEY as string} chainId={1}>
+        <div className="flex flex-col items-center">
         <DynamicWidget />
         <Signer/>
+        </div>
       </Shield3Provider>
       </div>
     </main>
