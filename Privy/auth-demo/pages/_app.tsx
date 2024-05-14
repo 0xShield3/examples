34aa13dd471a685278fa9eed90dde785823b3573
Isaac Patka <ipatka@gmail.com>
2024-05-14 16:18:17 -0400

    fix app.tsx

--- Privy/auth-demo/pages/_app.tsx
+++ Privy/auth-demo/pages/_app.tsx
@@ -50,7 +50,7 @@ function MyApp({Component, pageProps, router}: AppProps) {
         description="A simple library to onboard all your users to your web3 product, newcomers and experts alike."
       />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
         rel="stylesheet"
